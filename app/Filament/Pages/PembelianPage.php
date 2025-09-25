f6ba856ee0d2e60cc5bf2940efba45a5da45b193
ahmadakasah19-coder <ahmadakasah19@gmail.com>
2025-09-25 12:18:29 +0800

    model

--- app/Filament/Pages/PembelianPage.php
+++ app/Filament/Pages/PembelianPage.php
@@ -2,11 +2,28 @@
 
 namespace App\Filament\Pages;
 
+use App\Models\Barang;
 use Filament\Pages\Page;
+use App\Models\Pembelian;
+use Filament\Actions\Action;
+use Illuminate\Support\Facades\DB;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Filament\Tables\Actions\DeleteAction;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Tables\Concerns\InteractsWithTable;
 
-class PembelianPage extends Page
+
+class PembelianPage extends Page implements HasTable, HasForms
 {
+    use InteractsWithForms, InteractsWithTable;
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.pembelian-page';
 }
+
